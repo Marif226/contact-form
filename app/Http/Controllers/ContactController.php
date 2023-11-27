aa1166111a418393bbe8a464a96f5104d1c29ca6
Marif226 <marif226@yandex.ru>
2023-11-27 21:43:11 +0600

    feat(contact): added contacts storing in db

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Contact;
 use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
     public function submit(ContactRequest $request) {
-        return "Okay";
+        $contact = new Contact();
+        $contact->name = $request->input('name');
+        $contact->email = $request->input('email');
+        $contact->message = $request->input('message');
+
+        $contact->save();
+
+        return redirect()->route('contact');
     }
 }
